45ca0a0350354809f40ea987779770e135d5f9f9
Ben Thayer <ben@benthayer.com>
2020-09-23 16:17:00 -0500

    genCommit works

--- gen.cpp
+++ gen.cpp
@@ -31,33 +31,40 @@ string hashObject(string strObject) {
 
 }
 
-string asdf() {
-    // const unsigned char str[] =
-    string commit(
-        "tree f9baff06993b951e53c0312442241c6ee30921a6\n"
-        "parent c9fc3d97717367b5fa45709a70a15ddd657f0275\n"
+string genCommit(string parentHash, int salt) {
+    string tree("tree f9baff06993b951e53c0312442241c6ee30921a6\n");
+    string content(
         "author Ben Thayer <ben@benthayer.com> 1600063486 -0500\n"
         "committer Ben Thayer <ben@benthayer.com> 1600063486 -0500\n"
         "\n"
-        "Added gen_commits to .gitignore\n"
-        "\n"
-        "0\n");
+        "Added gen_commits to .gitignore\n\n");
+
+    string parent("parent " + parentHash + "\n");
+
+    string commit;
+    commit.append(tree);
+    commit.append(parent);
+    commit.append(content);
+    commit.append(to_string(salt) + "\n");
 
     string object("commit ");
     object.append(to_string(commit.length()));
     object.push_back('\0');
-    object += commit;
+    object.append(commit);
 
     return hashObject(object);
 }
 
 int main() {
-    // string parent = "c9fc3d97717367b5fa45709a70a15ddd657f0275";
-    // commit hash is 2e835cf071ae2a3eda7482cca7a0177e79d1f047
+    // test to make sure things work
+    string testParentHash = "c9fc3d97717367b5fa45709a70a15ddd657f0275";
+    string testHash = "2e835cf071ae2a3eda7482cca7a0177e79d1f047";
+    if (genCommit(testParentHash, 0).compare(testHash)) {
+        cout << "Test commit has wrong hash! Exiting" << endl;
+        return 1;
+    }
 
-    // test commit
-    cout << asdf() << endl;
-    cout << "2e835cf071ae2a3eda7482cca7a0177e79d1f047" << endl;
+
 
 
     return 0;
