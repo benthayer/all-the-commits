a7bb448c2fe1a8d7d102bf2abe972ef5c3262db3
Jason Maldonado <platocrat@tuta.io>
2020-11-04 15:32:58 -0600

    need to thoroughly learn Error types

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,15 +2,17 @@ mod gen_commits;
 
 use gen_commits::gen_hash;
 use std::fs::File;
+use std::io::BufWriter;
 
-/// Cargo compiles main.rs first, so...
-/// Remember to run `cargo run main.rs` for the program to run properly!
-fn main() {
-  let (mut file, mut err) = (File::create("./commits"), File::create("./commits"));
+fn main() /* -> std::io::Result<()> */
+{
+  /// I really need to properly learn about Rust's `Result` and `Option` types:
+  /// http://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/first-edition/error-handling.html
+  let mut f = File::create("./commits")?;
+  Ok(());
   // check()
-  /// For the following line below, review mutex guards in Rust, see link below
-  /// for more info (make sure to CLT + F "defer" on the page):
-  /// https://www.reddit.com/r/golang/comments/5plu38/re_esrs_comparison_of_concurrency_in_go_and_rust/
-  // defer f.close()
 
+  // After thoroughly learning how to handle Error types, fix this by completing
+  // this function's return type.
+  let mut w = BufWriter::new(file);
 }
