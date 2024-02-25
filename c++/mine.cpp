080d9781f2bf203ff9f0c53d66b5559fc5130e25
Ben Thayer <ben@benthayer.com>
2024-02-25 13:03:17 -0800

    Updated mine.cpp to work with queues

--- c++/mine.cpp
+++ c++/mine.cpp
@@ -1,51 +1,69 @@
+// Example of how to use boost's lock free queue to synchronize threads
+// Compile with ++ mine.cpp hashUtils.cpp -o run -lssl -lcrypto -lpthread
 #include <cstring>
 #include <thread>
 #include <vector>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <atomic>
+
+#include <boost/lockfree/spsc_queue.hpp>
 
 #include "hashUtils.h"
 
 using namespace std;
 
-atomic<bool> stop_flag(false); // Atomic flag to signal threads to stop
+using queue = boost::lockfree::spsc_queue<bool>;
 
-void mine(int threadID) {
-  string strHash = "";
+void mine(int threadID, queue* myQueue, vector<queue*>* queues) {
+  string strHash = to_string(rand());
   unsigned char* hash;
   unsigned int x;
   
-  unsigned int target = 75;
+  unsigned int target = 80;
+
+  bool b;
 
   do {
+    if (myQueue->pop(b)) {
+      cout << "Received break signal: " << threadID << endl;
+      break;
+    }
     hash = hashObject(strHash);
     x = hashToInt(hash, 7);
     strHash = hashToHexString(hash);
-  } while (x > target && !stop_flag); // Example condition, replace 10000 with desired threshold
+  } while (x > target); // Example condition, replace 10000 with desired threshold
 
   if (x <= target) {
     cout << "Thread " << threadID << " mined " << x << endl;
+    for (queue* theirQueue : *queues) {
+      theirQueue->push(true);
+    }
   }
-
-  // Trigger all threads to stop
-  stop_flag = true;
 
   cout << "Thread " << threadID << " stopped" << endl;
 }
 
 
 int main() {
+  srand(time(0));
+
   // Define number of threads
-  const int num_threads = 1; // Example: 4 threads
+  const int num_threads = 2; // Example: 4 threads
 
   cout << "Running with " << num_threads << " threads" << endl;
 
   // Vector to store thread objects
+  vector<queue*> queues;
   vector<thread> threads;
 
   // Start threads
   for (int i = 0; i < num_threads; ++i) {
-    threads.push_back(thread(mine, i));
+    // queue myQueue(10);
+    queue* q = new queue(10);
+    queues.push_back(q);
+    threads.emplace_back(mine,i, queues[i], &queues);
   }
 
   // Join threads
